39668db23072668477d91e72f09149e6caa306e9
jalal mahabadi <jalalmahabadi8@gmail.com>
2025-07-21 18:48:38 +0430

    creating templates

--- website/views.py
+++ website/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_view(request):
-    return HttpResponse('home page')
+    return render(request,'home.html')
 
 def about_view(request):
-    return HttpResponse('about page')
+     return render(request, 'about.html')
 
 def contact_view(request):
-    return HttpResponse('contact page')
+     return render(request, 'contact.html')
